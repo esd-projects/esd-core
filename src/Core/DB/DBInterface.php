6f3c6c3ae20c8da43202c174c8c507b5f182c5f8
zhangjincheng <zhangjincheng@huidr.com>
2019-06-04 15:47:32 +0800

    event重构

---
+++ src/Core/DB/DBInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 白猫
+ * Date: 2019/6/4
+ * Time: 15:45
+ */
+
+namespace ESD\Core\DB;
+
+
+interface DBInterface
+{
+    public function query($query);
+}

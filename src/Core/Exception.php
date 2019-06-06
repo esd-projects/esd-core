fe355ae90e14002fcff79803c8944a32650bebc0
zhangjincheng <zhangjincheng@huidr.com>
2019-06-06 16:03:39 +0800

    event重构

--- src/Core/Exception.php
+++ src/Core/Exception.php
@@ -9,9 +9,19 @@
 namespace ESD\Core;
 
 
+use Throwable;
+
 class Exception extends \Exception
 {
     protected $trace = true;
+
+    protected $time;
+
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->time = (int)(microtime(true) * 1000 * 1000);
+    }
 
     /**
      * @return bool
@@ -29,5 +39,11 @@ class Exception extends \Exception
         $this->trace = $trace;
     }
 
-
+    /**
+     * @return int
+     */
+    public function getTime(): int
+    {
+        return $this->time;
+    }
 }

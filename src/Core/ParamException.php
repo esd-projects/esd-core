270c227f6e8add393e178fd2050c974a92de9540
zhangjincheng <zhangjincheng@huidr.com>
2019-06-06 14:56:37 +0800

    event重构

--- src/Core/ParamException.php
+++ src/Core/ParamException.php
@@ -7,8 +7,20 @@
  */
 
 namespace ESD\Core;
-use ESD\Core\Exception;
+
+use Throwable;
 
 class ParamException extends Exception
 {
+    /**
+     * RouteException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->setTrace(false);
+    }
 }
